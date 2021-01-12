29236798671d4e4fd0d65148b8e7aee5f38059a7
dalewang1995 <dalewang1995@gmail.com>
2021-01-12 23:16:31 +0800

    fix: ui color

--- src/pages/ToolPage.vue
+++ src/pages/ToolPage.vue
@@ -5,10 +5,10 @@
 
 <template>
   <section class="tool-module">
-    <div>工具箱模块{{title}}</div>
+    <div>工具箱模块</div>
     <div class="content">
         <div class="left">
-            <textarea v-model="textVal"></textarea>
+            <textarea v-model="textVal" placeholder="请输入需要转码的URL"></textarea>
         </div>
         <div class="right">{{resVal}}</div>
     </div>
@@ -68,6 +68,7 @@ export default {
             padding: 10px;
             word-break: break-all;
             line-height: 20px;
+            background-color: #fff;
         }
 
         textarea {
@@ -84,12 +85,14 @@ export default {
     .op-con {
         .btn {
             background-color: #eee;
-            padding: 8px;
+            padding: 6.5px 15px; 
             border:none;
             border-radius: 3px;
+            transition: all .3s cubic-bezier(.645,.045,.355,1);
 
             &:hover {
                 background-color: #8590a6;
+                color: #fff;
             }
             &:first-child {
                 margin-right: 35px;
