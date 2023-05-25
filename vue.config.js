d5819c648927eeb29405676083347f648cb15a2c
coderw <dalewang1995@gmail.com>
2023-05-25 21:47:06 +0800

    feat: add docs

--- vue.config.js
+++ vue.config.js
@@ -11,6 +11,10 @@ function resolveRealPath(dir) {
 }
 
 module.exports = {
+  	// 基本路径
+	publicPath: './',
+	// 输出文件目录(默认dist)
+	outputDir: 'docs',
   productionSourceMap: false,
   css: {
     loaderOptions: {
